0511985c96f4c1ceb9bc8cad3d45f77bc3b68da5
Timothy Tsui <timiscoding@gmail.com>
2016-01-15 22:08:39 +1100

    Fixed seed data. Added score to tasks view. Added pry gem.

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,49 +43,49 @@ a6 = Activity.create :title => "changing light globe", :effort => "1"
 a7 = Activity.create :title => "empting changing bins", :effort => "1"
 a8 = Activity.create :title => "dusting", :effort => "2"
 
-a1.group = g1
-a2.group = g1
-a3.group = g1
-a4.group = g1
-a5.group = g2
-a6.group = g2
-a7.group = g2
-a8.group = g2
+a1.update_attribute :group, g1
+a2.update_attribute :group, g1
+a3.update_attribute :group, g1
+a4.update_attribute :group, g1
+a5.update_attribute :group, g2
+a6.update_attribute :group, g2
+a7.update_attribute :group, g2
+a8.update_attribute :group, g2
 
-a1.user = u2
-a2.user = u3
-a3.user = u1
-a4.user = u4
-a5.user = u5
-a6.user = u6
-a7.user = u7
-a8.user = u8
+a1.update_attribute :user, u2
+a2.update_attribute :user, u3
+a3.update_attribute :user, u1
+a4.update_attribute :user, u4
+a5.update_attribute :user, u5
+a6.update_attribute :user, u6
+a7.update_attribute :user, u7
+a8.update_attribute :user, u8
 
 Task.destroy_all
 
-t1 = Task.create :due_date => '2016-02-11', :done => true, :score => '1' 
-t2 = Task.create :due_date => '2016-02-13', :done => true, :score => '2'
-t3 = Task.create :due_date => '2016-02-15', :done => false, :score => '3'
-t4 = Task.create :due_date => '2016-02-23', :done => false, :score => '4'
-t5 = Task.create :due_date => '2016-01-01', :done => true, :score => '3' 
-t6 = Task.create :due_date => '2016-01-08', :done => true, :score => '2'
-t7 = Task.create :due_date => '2016-01-15', :done => true, :score => '3'
-t8 = Task.create :due_date => '2016-01-22', :done => true, :score => '1'
+t1 = Task.create :due_date => '2016-02-11', :done => true, :score => 1
+t2 = Task.create :due_date => '2016-02-13', :done => true, :score => 2
+t3 = Task.create :due_date => '2016-02-15', :done => false, :score => 3
+t4 = Task.create :due_date => '2016-02-23', :done => false, :score => 4
+t5 = Task.create :due_date => '2016-01-01', :done => true, :score => 3
+t6 = Task.create :due_date => '2016-01-08', :done => true, :score => 2
+t7 = Task.create :due_date => '2016-01-15', :done => true, :score => 3
+t8 = Task.create :due_date => '2016-01-22', :done => true, :score => 1
 
-t1.user = u1
-t2.user = u2
-t3.user = u2
-t4.user = u3
-t5.user = u3 
-t6.user = u4
-t7.user = u5
-t8.user = u5
+t1.update_attribute :user, u1
+t2.update_attribute :user, u2
+t3.update_attribute :user, u2
+t4.update_attribute :user, u3
+t5.update_attribute :user, u3
+t6.update_attribute :user, u4
+t7.update_attribute :user, u5
+t8.update_attribute :user, u5
 
-t1.activity = a1 
-t2.activity = a2
-t3.activity = a3
-t4.activity = a4
-t5.activity = a1 
-t6.activity = a2
-t7.activity = a3
-t8.activity = a4
+t1.update_attribute :activity, a1
+t2.update_attribute :activity, a2
+t3.update_attribute :activity, a3
+t4.update_attribute :activity, a4
+t5.update_attribute :activity, a1
+t6.update_attribute :activity, a2
+t7.update_attribute :activity, a3
+t8.update_attribute :activity, a4
