4734db69514fd75463880c3255eddec0ee566007
Timothy Tsui <timiscoding@gmail.com>
2016-01-18 10:42:38 +1100

    global var app.current_user.group. /groups returns groups for current user. /groups/1 returns all users belonging to that group.

--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -8,8 +8,8 @@ app.AppView = Backbone.View.extend({
     var taskViewTemplate = "taskViewTemplate";
     this.$el.append(taskViewTemplate);
 
-    var memberListView = new app.MemberListView();
-    this.$el.append( memberListView.el );
+    // var memberListView = new app.MemberListView();
+    // this.$el.append( memberListView.el );
 
     var appViewTemplate = "anything";
     this.$el.append(appViewTemplate);
