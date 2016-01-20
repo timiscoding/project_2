8ae768fa3cf6c81eeea826de56b73f70f96f4d42
SUGIMURAWATARU <sugimura.wataru@gmail.com>
2016-01-21 10:22:41 +1100

    progress on feedbacklist

--- app/assets/javascripts/views/FeedbackListView.js
+++ app/assets/javascripts/views/FeedbackListView.js
@@ -9,23 +9,32 @@ app.FeedbackListView = Backbone.View.extend({
     this.$el.html( appViewHTML );
 
     var currentUsersGroupId = app.current_user.group.id
-    // this is going through checking each feedback and grab appropriate feedbacks.
-    var currentUsersTasks = this.collection.select(function (model) {
-      // task done or not? You need to add into json.jbuilder file as well.
-      var taskStatus = this.model.get("done"); //true or false
-      console.log( taskStatus );
-      // user give rating or not.
-      var ratingStatus = this.model.get("rating");
-      var groupIdOfFeedback = this.model.get("group_id")
-      
-      // testing!!! need to include one omre condition && ratingStatus === ""
-      // if ( groupIdOfFeedback === currentUsersGroupId && taskStatus === true ) {
-        var feedbackListView = new app.FeedbackListItemView({model: model});
-        feedbackListView.render();
-      //   return true;
-      // } else {
-      //   return false;
-      // }
+    // this is going through checking each feedback and grab appropriate feedbacks.]
+    var view = this; // Make sure you have a reference to the current view (the word this will be redefined in the done handler of the fetch)
+
+    this.collection.fetch().done(function () {
+      // Go through the collection and store the current model as model
+      var currentUsersFeedbacks = view.collection.each( function (model) { 
+        // task done or not? You need to add into json.jbuilder file as well.
+        var taskStatus = model.get("done"); //true or false
+        // user rated to the task or not.
+        var ratingStatus = model.get("rating");
+        var groupIdOfFeedback = model.get("group_id")
+
+        if ( groupIdOfFeedback === currentUsersGroupId 
+          && taskStatus === true && ratingStatus === null ) {
+          // Show a view for the current model in the each loop
+          var feedbackListView = new app.FeedbackListItemView({model: model}); 
+          feedbackListView.render();
+
+          return true;
+
+        } else {
+
+          return false;
+
+        }
+      });
     });
   }
 });
--- app/views/feedbacks/index.json.jbuilder
+++ app/views/feedbacks/index.json.jbuilder
@@ -3,6 +3,6 @@ json.array!(@feedbacks) do |feedback|
   json.url feedback_url(feedback, format: :json)
   json.done feedback.task.done
   json.group_id feedback.task.activity.group_id
-  json.title feedback.task.activity.title 
-end
+  json.title feedback.task.activity.title
+  json.first_name feedback.user.first_name  
 end
