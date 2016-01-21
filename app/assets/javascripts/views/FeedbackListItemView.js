90b5554fdc67c810a1eeb02004b41c05c91fa8c4
SUGIMURAWATARU <sugimura.wataru@gmail.com>
2016-01-22 09:39:39 +1100

    adding some comments

--- app/assets/javascripts/views/FeedbackListItemView.js
+++ app/assets/javascripts/views/FeedbackListItemView.js
@@ -6,10 +6,10 @@ app.FeedbackListItemView = Backbone.View.extend({
     //'click .toggle': 'toggle',
     'click button': 'giveNewFeedback'
   },
-  //Click button 0 or 1 on feedback and save the rating to saver.
+  //Click button 0 or 1 on feedback and save the rating to the saver.
   giveNewFeedback: function (event) {
-    var $currentEl = $(event.currentTarget); //get the current element
-    var desiredRating = $currentEl.attr("id"); //get the rating score of current feedback. Here, rating and ID are same. 0 or 1.
+    var $currentEl = $(event.currentTarget); //get the currentTarget element
+    var desiredRating = $currentEl.attr("id"); //get the rating score of current feedback. Here, rating and ID are same. 0 or 1. you can refer to the app.html.erb
     var taskID = this.model.get("task_id"); //get the task ID related to the feedback
     var userID = app.current_user.id; //current user ID
 
@@ -19,13 +19,12 @@ app.FeedbackListItemView = Backbone.View.extend({
       user_id: userID, //user_id = userID
       rating: desiredRating || 0 // id rating is "null", put 0. to avoid error. jsut in case.
     });
-    // to write code below, you do not need to refresh
+    // Thanks to the code below, you do not need to refresh by hand.
     var thisEl = this.$el;
     feedback.save().done(function () {
       thisEl.remove();
     });
   },
-
 
   // here is doing for a feedback.
   render: function(){
@@ -35,7 +34,7 @@ app.FeedbackListItemView = Backbone.View.extend({
 
     var templateDetails = this.model.toJSON();
 
-    // rating = latest updated rating by a user( this is not average one.)
+    // this one : rating = latest updated rating by a user( this is not average one.)
     //templateDetails.rating = _.last(feedback).get("rating");
 
     var feedbacks = app.feedbacks.where({ task_id: this.model.get("task_id") });
@@ -43,14 +42,15 @@ app.FeedbackListItemView = Backbone.View.extend({
       return feedback.get("rating");
     });
 
-    // calculate sum of all of the rating by eahc users
+    // calculate sum of all of the rating by each users
     var sumOfRatings = _.reduce(feedbackRatingArray, function (sum, num) {
+      // to avoid including the rating -1 which is default value.
       if (num >= 0) {
         return sum + num;
       }
       return sum;
     });    
-    // average_rating = divided by the number of rating by each user.
+    // average_rating = sum of the rating score divided by the number of rating.
     templateDetails.average_rating = sumOfRatings / feedbackRatingArray.length;
 
     this.$el.html( feedbackListViewTemplater( templateDetails ));
--- app/assets/javascripts/views/FeedbackListView.js
+++ app/assets/javascripts/views/FeedbackListView.js
@@ -9,13 +9,13 @@ app.FeedbackListView = Backbone.View.extend({
     this.$el.html( appViewHTML );
 
     var currentUsersGroupId = app.current_user.group.id
-    // this is going through checking each feedback and grab appropriate feedbacks.]
-    var view = this; // Make sure you have a reference to the current view (the word this will be redefined in the done handler of the fetch)
+    // this is going through checking each feedback and grab appropriate feedbacks.
+    var view = this; // Make sure you have a reference to the current view (the word "this" will be redefined in the done handler of the fetch)
 
     this.collection.fetch().done(function () {
       // Go through the collection and store the current model as model
       var currentUsersFeedbacks = view.collection.each( function (model) { 
-        // task done or not? You need to add into json.jbuilder file as well.
+        // task done or not? You need to add into json.jbuilder file as well becasuse task model does not have "rating".
         var taskStatus = model.get("done"); //true or false
         // user rated to the task or not.
         var ratingStatus = model.get("rating");
@@ -25,8 +25,8 @@ app.FeedbackListView = Backbone.View.extend({
 
         if ( groupIdOfFeedback === currentUsersGroupId 
           && taskStatus === true 
-          && model.get("user_id") !== app.current_user.id
-          && app.feedbacks.where({ 
+          && model.get("user_id") !== app.current_user.id //current user cannot rate for the ownn task.
+          && app.feedbacks.where({ //make sure to show feedbacks that crrent user has never rated.
             task_id: model.get("task_id"), 
             user_id: app.current_user.id 
           }).length === 0) {
