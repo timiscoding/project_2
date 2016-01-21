5a5ddb86af6dbedf1b0d8d9b97b5a68129bbb4a5
Robert Murray <robert.b.murray@me.com>
2016-01-21 14:09:39 +1100

    negative message

--- app/assets/javascripts/views/NegativeMessageView.js
+++ app/assets/javascripts/views/NegativeMessageView.js
@@ -7,19 +7,30 @@ app.NegativeMessageView = Backbone.View.extend({
     'click #doTomorrow': 'doTomorrow'
   },
 
-  doTomorrow: function () {
+  initialize: function () {
+    var negativeResponse;
+  },
 
+  doTomorrow: function () {
+    console.log(negativeResponse);
   },
 
   render: function () {
-    // var NegativeMessageViewTemplate = _.template($('#NegativeMessageViewTemplate').html());
-    // this.$el.html(NegativeMessageViewTemplate);
-    // app.tasks = new app.Tasks();
-    // app.tasks.fetch().done(function () {
-    //   app.userFeedbacks = new app.userFeedbacks(null, {user_id: app.current_user.id});
-    //   app.userFeedbacks.fetch().done(function () {
-    //   })
-    // }
+    var messageShowing;
+    var NegativeMessageViewTemplate = _.template($('#NegativeMessageViewTemplate').html());
+    this.$el.html(NegativeMessageViewTemplate);
+    app.tasks = new app.Tasks();
+    app.tasks.fetch().done(function () {
+      app.userFeedbacks = new app.UserFeedbacks(null, {user_id: app.current_user.id});
+      app.userFeedbacks.fetch().done(function () {
+        console.log('my feedbacks', app.userFeedbacks);
+        if (!messageShowing) {
+          messageShowing = true;
+          negativeResponse = app.userFeedbacks.where({ rating: 0 })[0];
+          $('#taskTitle').html(negativeResponse.get("task").title)
+        }
+      });
+    });
   }
 
 
